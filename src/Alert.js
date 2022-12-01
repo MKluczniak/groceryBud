b046b18b84333e02a534e0412f402662eaf51577
Mikolaj Kluczniak <mikolajkluczniak@MacBook-Pro-Mikolaj.local>
2022-12-01 12:24:19 +0100

    pruductes sotred successfully in the local storage

--- src/Alert.js
+++ src/Alert.js
@@ -1,7 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react"
 
-const Alert = () => {
-  return <h2>alert component</h2>
+const Alert = ({ type, msg, removeAlert, list }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert()
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [list]) //every time list is updated the useEffect runes again so the "clear list alert is showed for 3 sec, not for the reminding time of the last display" once sth happens to the list we clear out the old setTimeout and get new one
+
+  return <p className={`alert alert-${type}`}>{msg}</p>
 }
 
 export default Alert
